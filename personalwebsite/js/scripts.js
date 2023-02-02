a44931fe6ed1d54eea58d9b96331121e325c72c9
Hayden Paoletti <hpaoletti11@gmail.com>
2023-02-02 01:12:14 -0700

    hamburgler

--- personalwebsite/js/scripts.js
+++ personalwebsite/js/scripts.js
@@ -1,5 +1,13 @@
 $(".navClass").hide();
 
-$(".navButton").click(function () {
-  $(".navClass").fadeToggle( 600, "swing");
+
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function() {
+  $hamburger.toggleClass("is-active");
+  // Do something else, like open/close menu
 });
+
+
+$(".hamburger").click(function () {
+  $(".navClass").fadeToggle( 500, "swing");
+})
